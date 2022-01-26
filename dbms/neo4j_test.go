a0d54d8364966749cbda2c0b6bdf6077bdd7c998
mcauto <nexters@kakao.com>
2022-01-27 07:21:47 +0900

    ci: neo4j test container 추가

--- dbms/neo4j_test.go
+++ dbms/neo4j_test.go
@@ -4,29 +4,92 @@ import (
 	"checkareer-core/_test"
 	"checkareer-core/config"
 	"checkareer-core/dbms"
+	"context"
+	"fmt"
+	"log"
 	"testing"
 
 	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 	"go.uber.org/fx"
 )
 
+// registerHook invoke로 호출되므로 파라미터의 순서가 초기화의 순서
+func registerHook(
+	lifecycle fx.Lifecycle,
+	neo4jContainer testcontainers.Container,
+	driver neo4j.Driver,
+) {
+	lifecycle.Append(fx.Hook{
+		OnStop: func(context context.Context) error {
+			err := driver.Close()
+			if err != nil {
+				fmt.Println(err)
+			}
+			return neo4jContainer.Terminate(context)
+		},
+	})
+}
+
+func NewNeo4jTestContainer(settings *config.Settings) testcontainers.Container {
+	ctx := context.Background()
+	container, err := startContainer(ctx, settings.Neo4j.Username, settings.Neo4j.Password)
+	if err != nil {
+		log.Fatal(err)
+	}
+	port, err := container.MappedPort(ctx, "7687")
+	if err != nil {
+		log.Fatal(err)
+	}
+	settings.Neo4j.URI = fmt.Sprintf("neo4j://localhost:%d", port.Int())
+	return container
+}
+
+func startContainer(
+	ctx context.Context,
+	username, password string,
+) (testcontainers.Container, error) {
+	request := testcontainers.ContainerRequest{
+		Image:        "neo4j",
+		ExposedPorts: []string{"7687/tcp"},
+		Env: map[string]string{
+			"NEO4J_AUTH": fmt.Sprintf("%s/%s", username, password),
+		},
+		WaitingFor: wait.ForLog("hi"),
+	}
+	return testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: request,
+		Started:          true,
+	})
+}
+
+func NewSettings() *config.Settings {
+	var settings config.Settings
+	settings.Neo4j.URI = ""
+	settings.Neo4j.Username = "neo4j"
+	settings.Neo4j.Password = "t2st2r"
+	return &settings
+}
+
 var TestModules = fx.Options(
-	config.Modules,
-	dbms.Modules,
+	fx.Provide(
+		dbms.NewNeo4jDriver,
+		dbms.NewNeo4jSessionGenerator,
+		NewSettings,
+		NewNeo4jTestContainer,
+	),
+	fx.Invoke(registerHook),
 )
 
 func TestCreateCyper(t *testing.T) {
 	f := func(settings *config.Settings, generator dbms.Neo4jSessionGenerator) {
-		if _, exist := settings.Extras["CI"]; exist {
-			return
-		}
 		session := generator()
 		defer session.Close()
 		result, err := session.WriteTransaction(createItem)
 		assert.NoError(t, err)
 		assert.NotNil(t, result)
-
 	}
 	app := _test.NewForTest(t, TestModules, fx.Invoke(f))
 	app.RequireStart()
